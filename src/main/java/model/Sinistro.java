b7c80398c2f850818b14750cd0b213a30a6c24e0
Thiago <thiagllima26@gmail.com>
2024-10-17 21:49:06 -0300

    Entidades atualizadas

--- src/main/java/model/Sinistro.java
+++ src/main/java/model/Sinistro.java
@@ -1,4 +1,21 @@
 package model;
 
 public class Sinistro {
+    private int id;
+    private String tipoEvento;
+    private double valorDanos;
+
+    public Sinistro(String tipoEvento, double valorDanos) {
+        this.tipoEvento = tipoEvento;
+        this.valorDanos = valorDanos;
+    }
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getTipoEvento() { return tipoEvento; }
+    public void setTipoEvento(String tipoEvento) { this.tipoEvento = tipoEvento; }
+    public double getValorDanos() { return valorDanos; }
+    public void setValorDanos(double valorDanos) { this.valorDanos = valorDanos; }
 }
+
