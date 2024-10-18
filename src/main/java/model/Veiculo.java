2766b38fe889872b8f11bd6462a9c304f24314c1
edu1805 <eduardobarriviera1805@gmail.com>
2024-10-17 21:42:12 -0300

    Classe Veiculo atualizado

--- src/main/java/model/Veiculo.java
+++ src/main/java/model/Veiculo.java
@@ -1,8 +1,22 @@
-package model;
-
 public class Veiculo {
-
-    private String marca;
+    private int id;
     private String modelo;
-    private int ano;
+    private int anoFabricacao;
+    private double valorInicial;
+
+    public Veiculo(String modelo, int anoFabricacao, double valorInicial) {
+        this.modelo = modelo;
+        this.anoFabricacao = anoFabricacao;
+        this.valorInicial = valorInicial;
+    }
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+    public int getAnoFabricacao() { return anoFabricacao; }
+    public void setAnoFabricacao(int anoFabricacao) { this.anoFabricacao = anoFabricacao; }
+    public double getValorInicial() { return valorInicial; }
+    public void setValorInicial(double valorInicial) { this.valorInicial = valorInicial; }
 }
